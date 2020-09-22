1728c5fccac59a96fd4e66d230a68e3f17d5f833
Michael Strasser <mstrass@thoughtworks.com>
2020-09-22 18:23:51 +1000

    More Item tests

--- cart/Item.test.js
+++ cart/Item.test.js
@@ -26,5 +26,22 @@ describe(Item, () => {
       const item = new Item(TEST_PRODUCT);
       expect(item.getTotal()).toEqual(0.0);
     });
+    it("is the product price when added to it", () => {
+      const item = new Item(TEST_PRODUCT);
+      item.addOne();
+      expect(item.getTotal()).toEqual(TEST_PRODUCT.getPrice());
+    });
+    it("is twice the product price when a second one is added to it", () => {
+      const item = new Item(TEST_PRODUCT);
+      item.addOne();
+      item.addOne();
+      expect(item.getTotal()).toEqual(2 * TEST_PRODUCT.getPrice());
+    });
+    it("is zero when added to it and then removed", () => {
+      const item = new Item(TEST_PRODUCT);
+      item.addOne();
+      item.removeOne();
+      expect(item.getTotal()).toEqual(0.0);
+    });
   });
 });
