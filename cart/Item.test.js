68620f91066afdc5bb14d55f45d84a8e82050975
Michael Strasser <mstrass@thoughtworks.com>
2020-09-22 18:35:21 +1000

    Use toBe() for numbers; test() instead of it()

--- cart/Item.test.js
+++ cart/Item.test.js
@@ -5,43 +5,43 @@ const TEST_PRODUCT = new Product("test", "Test item", 10.00);
 
 describe(Item, () => {
   describe("getCount()", () => {
-    it("is zero when the Item is created", () => {
+    test("is zero when the Item is created", () => {
       const item = new Item(TEST_PRODUCT);
-      expect(item.getCount()).toEqual(0);
+      expect(item.getCount()).toBe(0);
     });
-    it("is 1 when a product is added to it", () => {
+    test("is 1 when a product is added to it", () => {
       const item = new Item(TEST_PRODUCT);
       item.addOne();
-      expect(item.getCount()).toEqual(1);
+      expect(item.getCount()).toBe(1);
     });
-    it("is 0 when a product is added to it and then removed", () => {
+    test("is 0 when a product is added to it and then removed", () => {
       const item = new Item(TEST_PRODUCT);
       item.addOne();
       item.removeOne();
-      expect(item.getCount()).toEqual(0);
+      expect(item.getCount()).toBe(0);
     });
   });
   describe("getTotal()", () => {
-    it("is zero when the Item is created", () => {
+    test("is zero when the Item is created", () => {
       const item = new Item(TEST_PRODUCT);
-      expect(item.getTotal()).toEqual(0.0);
+      expect(item.getTotal()).toBe(0.0);
     });
-    it("is the product price when added to it", () => {
-      const item = new Item(TEST_PRODUCT);
-      item.addOne();
-      expect(item.getTotal()).toEqual(TEST_PRODUCT.getPrice());
-    });
-    it("is twice the product price when a second one is added to it", () => {
+    test("is the product price when added to it", () => {
       const item = new Item(TEST_PRODUCT);
       item.addOne();
-      item.addOne();
-      expect(item.getTotal()).toEqual(2 * TEST_PRODUCT.getPrice());
+      expect(item.getTotal()).toBe(TEST_PRODUCT.getPrice());
     });
-    it("is zero when added to it and then removed", () => {
+    test("is twice the product price when a second one is added to it", () => {
+      const item = new Item(TEST_PRODUCT);
+      item.addOne();
+      item.addOne();
+      expect(item.getTotal()).toBe(2 * TEST_PRODUCT.getPrice());
+    });
+    test("is zero when added to it and then removed", () => {
       const item = new Item(TEST_PRODUCT);
       item.addOne();
       item.removeOne();
-      expect(item.getTotal()).toEqual(0.0);
+      expect(item.getTotal()).toBe(0.0);
     });
   });
 });
